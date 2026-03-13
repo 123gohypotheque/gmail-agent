58c803a62044757a27fa6292d6783fb8f77f6b93
123GoHypothèque <88048242+123gohypotheque@users.noreply.github.com>
2026-03-13 12:12:02 -0400

    Increase JSON and URL-encoded body size limits

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require("cors")
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
 // Variables d'environnement (sécurisées)
 const CLIENT_ID = process.env.CLIENT_ID
